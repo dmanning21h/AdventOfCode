21e190b1d3f507a787c799110d2567a0d21e70dc
Derek Manning <47646253+dmanning21h@users.noreply.github.com>
2024-12-02 09:29:19 -0500

    Finish Day02

--- 2024/Day02/Solution.cs
+++ 2024/Day02/Solution.cs
@@ -2,22 +2,184 @@
 
 namespace AdventOfCode.Y2024.Day02;
 
-public sealed class Solution : ISolution
+public sealed class Solution : BaseSolution, ISolution
 {
-    private string[] input;
-
-    public Solution(string rawInput)
+    public Solution(string rawInput) : base(rawInput)
     {
-        input = InputParser.ParseLines(rawInput);
     }
 
     public string SolvePartOne()
     {
-        return "Y2024 Day 02 Part 1";
+        int safeReportCount = 0;
+        foreach (string line in input)
+        {
+            List<int> report = line.Split(' ').Select(int.Parse).ToList();
+
+            if (IsReportValid(report))
+            {
+                safeReportCount++;
+            }
+        }
+
+        return safeReportCount.ToString();
     }
 
     public string SolvePartTwo()
     {
-        return "Y2024 Day 02 Part 2";
+        int safeReportCount = 0;
+        foreach (string line in input)
+        {
+            List<int> report = line.Split(' ').Select(int.Parse).ToList();
+
+            if (IsReportValidV2(report))
+            {
+                safeReportCount++;
+            }
+        }
+
+        return safeReportCount.ToString();
+    }
+
+    private bool IsReportValid(List<int> report)
+    {
+        var adjacentErrorIndexes = FindAdjacentErrorIndices(report);
+        var ascendingErrorIndexes = FindAscendingErrorIndices(report);
+        var descendingErrorIndexes = FindDescendingErrorIndices(report);
+
+        if ((ascendingErrorIndexes.Count > 0 && descendingErrorIndexes.Count > 0) || adjacentErrorIndexes.Count > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsReportValidV2(List<int> report)
+    {
+        var adjacentErrorIndexes = FindAdjacentErrorIndices(report);
+        var ascendingErrorIndexes = FindAscendingErrorIndices(report);
+        var descendingErrorIndexes = FindDescendingErrorIndices(report);
+
+        ascendingErrorIndexes.AddRange(adjacentErrorIndexes);
+        descendingErrorIndexes.AddRange(adjacentErrorIndexes);
+
+        foreach(var errorIndex in ascendingErrorIndexes.Distinct())
+        {
+            var reportCopy = new List<int>(report);
+            reportCopy.RemoveAt(errorIndex);
+            if (IsReportValid(reportCopy))
+            {
+                return true;
+            }
+        }
+
+        foreach (var errorIndex in descendingErrorIndexes.Distinct())
+        {
+            var reportCopy = new List<int>(report);
+            reportCopy.RemoveAt(errorIndex);
+            if (IsReportValid(reportCopy))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<int> FindAscendingErrorIndices(List<int> report)
+    {
+        List<int> errorIndexes = [];
+        for (int i = 0; i < report.Count; i++)
+        {
+            if (i == 0)
+            {
+                if (report[i] > report[i + 1])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else if (i == report.Count - 1)
+            {
+                if (report[i - 1] > report[i])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else
+            {
+                if (report[i - 1] > report[i] || report[i] > report[i + 1])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+        }
+
+        return errorIndexes;
+    }
+
+    private List<int> FindDescendingErrorIndices(List<int> report)
+    {
+        List<int> errorIndexes = [];
+        for (int i = 0; i < report.Count; i++)
+        {
+            if (i == 0)
+            {
+                if (report[i] < report[i + 1])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else if (i == report.Count - 1)
+            {
+                if (report[i - 1] < report[i])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else
+            {
+                if (report[i - 1] < report[i] || report[i] < report[i + 1])
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+        }
+
+        return errorIndexes;
+    }
+
+    private List<int> FindAdjacentErrorIndices(List<int> report)
+    {
+        List<int> errorIndexes = [];
+        for (int i = 0; i < report.Count; i++)
+        {
+            if (i == 0)
+            {
+                var absDiff = Math.Abs(report[i] - report[i + 1]);
+                if (absDiff < 1 || absDiff > 3)
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else if (i == report.Count - 1)
+            {
+                var absDiff = Math.Abs(report[i - 1] - report[i]);
+                if (absDiff < 1 || absDiff > 3)
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+            else
+            {
+                var absDiff1 = Math.Abs(report[i - 1] - report[i]);
+                var absDiff2 = Math.Abs(report[i] - report[i + 1]);
+                if (absDiff1 < 1 || absDiff1 > 3 || absDiff2 < 1 || absDiff2 > 3)
+                {
+                    errorIndexes.Add(i);
+                }
+            }
+        }
+
+        return errorIndexes;
     }
 }
