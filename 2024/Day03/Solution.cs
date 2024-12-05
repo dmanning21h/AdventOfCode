666120ed5193b189bd21d0a4089514dabc066758
dmanning21h <47646253+dmanning21h@users.noreply.github.com>
2024-12-04 18:16:52 -0600

    Finish Day 04

--- 2024/Day03/Solution.cs
+++ 2024/Day03/Solution.cs
@@ -1,23 +1,74 @@
 ﻿using AdventOfCode.Lib;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Y2024.Day03;
 
 public sealed class Solution : ISolution
 {
-    private string[] input;
+    private readonly string input;
 
     public Solution(string rawInput)
     {
-        input = InputParser.ParseLines(rawInput);
+        input = rawInput;
     }
 
     public string SolvePartOne()
     {
-        return "Y2024 Day 03 Part 1";
+
+        return GetSum(input).ToString();
     }
 
     public string SolvePartTwo()
     {
-        return "Y2024 Day 03 Part 2";
+        var firstMatch = Regex.Match(input, @"(.*?)don't[()]");
+
+        // Find the next match from this index
+        int startIndex = firstMatch.Index + firstMatch.Length;
+
+        int sum = GetSum(firstMatch.Groups[1].Value);
+
+        int matchesCount = 0;
+        while (startIndex < input.Length)
+        {
+            var match = Regex.Match(input.Substring(startIndex), @"do[()](.*?)don't[()]");
+            if (!match.Success)
+            {
+                break;
+            }
+            matchesCount++;
+
+            var text = match.Groups[1].Value;
+            sum += GetSum(text);
+
+            // Update startIndex to the end of the current match
+            startIndex += match.Index + match.Length;
+        }
+
+        // Check for remaining text after the last match
+        var remainingTextMatch = Regex.Match(input.Substring(startIndex), @"do[()](.*)");
+        if (remainingTextMatch.Success)
+        {
+            var remainingText = remainingTextMatch.Groups[1].Value;
+            sum += GetSum(remainingText);
+        }
+
+        return sum.ToString();
+    }
+
+    private int GetSum(string text)
+    {
+        Regex pattern = new Regex(@"mul[(](?<value1>\d+),(?<value2>\d+)[)]");
+        var match = pattern.Matches(text);
+
+        int sum = 0;
+        foreach (Match m in match)
+        {
+            var value1 = int.Parse(m.Groups["value1"].Value);
+            var value2 = int.Parse(m.Groups["value2"].Value);
+
+            sum += value1 * value2;
+        }
+
+        return sum;
     }
 }
