a25564d58b7327c129f919a445cfb640dea0bd01
dmanning21h <47646253+dmanning21h@users.noreply.github.com>
2024-12-06 06:18:15 -0600

    Parse input Day 05

--- 2024/Day05/Solution.cs
+++ 2024/Day05/Solution.cs
@@ -4,11 +4,15 @@ namespace AdventOfCode.Y2024.Day05;
 
 public sealed class Solution : ISolution
 {
-    private readonly string input;
+    private readonly string[] pageOrderingRules;
+    private readonly string[] pageUpdates;
 
     public Solution(string rawInput)
     {
-        input = rawInput;
+        var splitInput = rawInput.Split("\r\n\r\n");
+
+        pageOrderingRules = InputParser.ParseLines(splitInput[0]);
+        pageUpdates = InputParser.ParseLines(splitInput[1]);
     }
 
     public string SolvePartOne()
