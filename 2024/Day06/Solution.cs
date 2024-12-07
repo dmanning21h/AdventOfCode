0d058a716e2b5e37faa2c3952f6d36313427acd1
dmanning21h <47646253+dmanning21h@users.noreply.github.com>
2024-12-06 21:26:23 -0600

    Finish Day06

--- 2024/Day06/Solution.cs
+++ 2024/Day06/Solution.cs
@@ -4,128 +4,205 @@ namespace AdventOfCode.Y2024.Day06;
 
 public sealed class Solution : ISolution
 {
+    private readonly LabPatrol labPatrol;
 
-    public Solution(string rawInput)
+    public Solution(string rawInput) => labPatrol = new LabPatrol(rawInput);
+
+    public string SolvePartOne() => labPatrol.PatrolV1().ToString();
+
+    public string SolvePartTwo() => labPatrol.PatrolV2().ToString();
+
+    private sealed class LabPatrol
     {
-    }
+        public Grid Grid { get; init; }
 
-    public string SolvePartOne()
-    {
-        int answer = 0;
+        private Coordinate GuardCoordinate { get; set; }
 
-        return answer.ToString();
-    }
+        private Direction CurrentDirection { get; set; } = Direction.Up;
 
-    public string SolvePartTwo()
-    {
-        int answer = 0;
+        private bool IsGuardInLab => CoordinateIsInGrid(GuardCoordinate);
 
-        return answer.ToString();
-    }
+        private HashSet<(int x, int y)> visitedPositions = [];
+        private HashSet<(int x, int y, int dx, int dy)> visitedVectors = [];
 
-    private int GetMiddlePage(Manual manual)
-    {
-        return manual.PageNumbers[manual.PageNumbers.Count / 2];
-    }
-
-    private sealed class Manual
-    {
-        public List<int> PageNumbers { get; set; } = [];
-
-        public Manual(string rawManual)
+        public LabPatrol(string rawInput)
         {
-            PageNumbers = rawManual.Split(",").Select(int.Parse).ToList();
+            List<List<char>> charGrid = rawInput.Split("\r\n").Select(line => line.ToList()).ToList();
+
+            Grid = new Grid(charGrid);
+
+            GuardCoordinate = Grid.Coordinates.SelectMany(row => row).First(coord => coord.Value == '^');
         }
-    }
 
-
-    private sealed class PageOrderRule
-    {
-        public int PageNumber { get; set; }
-
-        public List<int> RequiredFuturePages { get; set; } = [];
-
-        public void AddRequiredFuturePage(int pageNumber)
+        public int PatrolV1()
         {
-            RequiredFuturePages.Add(pageNumber);
-        }
-    }
-
-    private sealed class PageOrderRuleCollection
-    {
-        public PageOrderRuleCollection(string[] input)
-        {
-            foreach (var rule in input)
+            while (IsGuardInLab)
             {
-                var splitRule = rule.Split("|");
-                var pageNumber = int.Parse(splitRule[0]);
-                var futurePageNumber = int.Parse(splitRule[1]);
+                int dx = GetDx(CurrentDirection);
+                int dy = GetDy(CurrentDirection);
 
-                AddRequiredFuturePage(pageNumber, futurePageNumber);
+                var nextMoveCoordinate = new Coordinate(GuardCoordinate.X + dx, GuardCoordinate.Y + dy);
+                if (CoordinateIsInGrid(nextMoveCoordinate))
+                {
+                    if (Grid[nextMoveCoordinate].Value == '#')
+                    {
+                        Turn();
+                        continue;
+                    }
+                }
+
+                visitedPositions.Add((GuardCoordinate.X, GuardCoordinate.Y));
+                visitedVectors.Add((GuardCoordinate.X, GuardCoordinate.Y, dx, dy));
+
+                GuardCoordinate = nextMoveCoordinate;
+            }
+
+            return visitedPositions.Count;
+        }
+
+        public int PatrolV2()
+        {
+            int obstructionPoints = 0;
+            GuardCoordinate = Grid.Coordinates.SelectMany(row => row).First(coord => coord.Value == '^');
+            var initialGuardPosition = (GuardCoordinate.X, GuardCoordinate.Y);
+
+            foreach (var (x, y) in visitedPositions)
+            {
+                if ((x, y) == initialGuardPosition)
+                {
+                    continue;
+                }
+
+                Grid.Coordinates[y][x].Value = '#';
+
+                GuardCoordinate = Grid.Coordinates.SelectMany(row => row).First(coord => coord.Value == '^');
+                CurrentDirection = Direction.Up;
+
+                var localVisitedVectors = new HashSet<(int x, int y, int dx, int dy)>();
+
+                while (IsGuardInLab)
+                {
+                    int dx = GetDx(CurrentDirection);
+                    int dy = GetDy(CurrentDirection);
+
+                    var nextMoveCoordinate = new Coordinate(GuardCoordinate.X + dx, GuardCoordinate.Y + dy);
+                    if (CoordinateIsInGrid(nextMoveCoordinate))
+                    {
+                        if (Grid[nextMoveCoordinate].Value == '#')
+                        {
+                            Turn();
+                            continue;
+                        }
+                    }
+
+                    var currentPos = (GuardCoordinate.X, GuardCoordinate.Y, GetDx(CurrentDirection), GetDy(CurrentDirection));
+                    if (localVisitedVectors.Contains(currentPos))
+                    {
+                        obstructionPoints++;
+                        break;
+                    }
+                    else
+                    {
+                        localVisitedVectors.Add(currentPos);
+                    }
+
+                    GuardCoordinate = nextMoveCoordinate;
+                }
+
+                Grid.Coordinates[y][x].Value = '.';
+            }
+
+            return obstructionPoints;
+        }
+
+        private void Turn()
+        {
+            CurrentDirection = CurrentDirection switch
+            {
+                Direction.Up => Direction.Right,
+                Direction.Right => Direction.Down,
+                Direction.Down => Direction.Left,
+                Direction.Left => Direction.Up,
+                _ => throw new InvalidOperationException("Invalid direction")
+            };
+        }
+
+        private int GetDx(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Right => 1,
+                Direction.Left => -1,
+                _ => 0
+            };
+        }
+
+        private int GetDy(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => -1,
+                Direction.Down => 1,
+                _ => 0
+            };
+        }
+
+        private bool CoordinateIsInGrid(Coordinate coordinate)
+        {
+            return (coordinate.X >= 0 && coordinate.X < Grid.Coordinates[0].Count) && (coordinate.Y >= 0 && coordinate.Y < Grid.Coordinates.Count);
+        }
+    }
+
+    private sealed record Grid
+    {
+        public List<List<Coordinate>> Coordinates { get; init; }
+
+        public Grid(List<List<char>> charGrid)
+        {
+            Coordinates = [];
+            foreach (var row in charGrid)
+            {
+                List<Coordinate> coords = [];
+                foreach (var item in row)
+                {
+                    int x = row.IndexOf(item);
+                    int y = charGrid.IndexOf(row);
+                    coords.Add(new Coordinate(x, y) { Value = item });
+                }
+
+                Coordinates.Add(coords);
             }
         }
 
-        public List<PageOrderRule> PageOrderRules { get; set; } = [];
-
-        public PageOrderRule this[int pageNumber]
+        public Coordinate this[Coordinate coordinate]
         {
             get
             {
-                var rule = PageOrderRules.FirstOrDefault(r => r.PageNumber == pageNumber);
-                if (rule == null)
-                {
-                    rule = new PageOrderRule { PageNumber = pageNumber };
-                    PageOrderRules.Add(rule);
-                }
-                return rule;
+                return Coordinates[coordinate.Y][coordinate.X];
             }
         }
+    }
 
-        public void AddRequiredFuturePage(int pageNumber, int invalidPageNumber)
+    public record Coordinate
+    {
+        public int X { get; init; }
+
+        public int Y { get; init; }
+
+        public char Value { get; set; }
+
+        public Coordinate(int x, int y)
         {
-            this[pageNumber].AddRequiredFuturePage(invalidPageNumber);
+            X = x;
+            Y = y;
         }
+    }
 
-        public bool IsValidPage(int currentPageNumber, int futurePageNumber)
-        {
-            return !this[futurePageNumber].RequiredFuturePages.Contains(currentPageNumber);
-        }
-
-        public bool IsValidManual(Manual manual)
-        {
-            var pageNumbers = manual.PageNumbers.ToArray();
-            for (int i = 0; i < pageNumbers.Length; i++)
-            {
-                for (int j = i + 1; j < pageNumbers.Length; j++)
-                {
-                    if (!IsValidPage(pageNumbers[i], pageNumbers[j]))
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
-
-        public Manual SortInvalidManual(Manual manual)
-        {
-            var pageNumbers = manual.PageNumbers.ToArray();
-            for (int i = 0; i < pageNumbers.Length; i++)
-            {
-                for (int j = i + 1; j < pageNumbers.Length; j++)
-                {
-                    if (!IsValidPage(pageNumbers[i], pageNumbers[j]))
-                    {
-                        var temp = pageNumbers[i];
-                        pageNumbers[i] = pageNumbers[j];
-                        pageNumbers[j] = temp;
-                    }
-                }
-            }
-
-            manual.PageNumbers = pageNumbers.ToList();
-            return manual;
-        }
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
     }
 }
