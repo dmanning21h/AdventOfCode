0da586425fca4415a45fbd6ddc00be137b8bf171
dmanning21h <47646253+dmanning21h@users.noreply.github.com>
2024-12-07 06:53:13 -0600

    Finish Day07

---
+++ 2024/Day08/Solution.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2024.Day08;
+
+public sealed class Solution : ISolution
+{
+
+    public Solution(string rawInput)
+    { }
+
+    public string SolvePartOne()
+    {
+        int answer = 0;
+
+        return answer.ToString();
+    }
+
+    public string SolvePartTwo()
+    {
+        int answer = 0;
+
+        return answer.ToString();
+    }
+}
